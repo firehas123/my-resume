4bde19e91db00b0cf042777b60b8d69ffd3210b8
firehas123 <mr.hassanch@gmail.com>
2024-05-31 05:55:32 +0200

    backend proj added

---
+++ resumeb/src/main/java/com/resumeb/ResumebApplication.java
@@ -0,0 +1,13 @@
+package com.resumeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumebApplication.class, args);
+	}
+
+}
---
+++ resumeb/src/test/java/com/resumeb/ResumebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
