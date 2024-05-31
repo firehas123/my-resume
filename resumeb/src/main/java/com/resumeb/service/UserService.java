2b1df71eaefce34800c3c0d5b9c4bcaa902070f7
firehas123 <mr.hassanch@gmail.com>
2024-05-31 07:27:36 +0200

    misc: added docker files

---
+++ resumeb/src/main/java/com/resumeb/service/UserService.java
@@ -0,0 +1,19 @@
+package com.resumeb.service;
+
+import com.resumeb.model.User;
+import com.resumeb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
+
--- resumef/src/App.js
+++ resumef/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + '/users')
+            .then(response => response.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
