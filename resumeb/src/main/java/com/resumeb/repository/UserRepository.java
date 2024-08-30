386d791a9ad23de16279d12ae34069b120568df9
firehas123 <mr.hassanch@gmail.com>
2024-08-30 19:34:24 +0200

    init: project initialization done:

--- resumeb/src/main/java/com/resumeb/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.resumeb.repository;
-
-import com.resumeb.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
